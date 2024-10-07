871d301568fc75b7eb924f6de2b9f545722c62be
Flavio Alvarenga <flavio.alva@outlook.com>
2024-10-07 07:50:26 -0300

    adicionar carta de disciplina no app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <script setup lang="ts">
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardTitle,
-  CardHeader,
-  CardFooter,
-} from "./components/ui/card";
+import CardDisciplina from "./components/card-disciplina.vue";
 </script>
 
 <template>
   <div class="flex justify-center mt-10">
-    <Card class="bg-background max-w-[40rem]">
-      <CardHeader>
-        <CardTitle> Título da Carta </CardTitle>
-        <CardDescription class="ml-[2px]"> Descrição da carta </CardDescription>
-      </CardHeader>
-      <CardContent>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio laborum
-        temporibus ipsa excepturi alias praesentium iure voluptatum perspiciatis
-        inventore assumenda sunt sit quidem ullam, rem consequatur. Commodi est
-        repudiandae ducimus?</CardContent
-      >
-      <CardFooter> footer da carta </CardFooter>
-    </Card>
+    <div class="flex flex-col gap-2">
+      <CardDisciplina />
+      <CardDisciplina />
+      <CardDisciplina />
+    </div>
   </div>
 </template>
 
