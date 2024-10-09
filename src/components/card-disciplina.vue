f3e8c47b4c054c8c07531ef4836de6e5cd06759a
Flavio Alvarenga <flavio.alva@outlook.com>
2024-10-09 11:56:20 -0300

    feat: adicionar Slider no Card da disciplina

--- src/components/card-disciplina.vue
+++ src/components/card-disciplina.vue
@@ -3,24 +3,30 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardTitle,
-  CardHeader,
   CardFooter,
+  CardHeader,
+  CardTitle,
 } from "./ui/card";
+import { Slider } from "./ui/slider";
 </script>
 
 <template>
-  <Card class="bg-background max-w-[40rem]">
+  <Card class="bg-card max-w-[40rem]">
     <CardHeader>
       <CardTitle> Título da Carta </CardTitle>
       <CardDescription class="ml-[2px]"> Descrição da carta </CardDescription>
     </CardHeader>
+
     <CardContent>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio laborum
       temporibus ipsa excepturi alias praesentium iure voluptatum perspiciatis
       inventore assumenda sunt sit quidem ullam, rem consequatur. Commodi est
-      repudiandae ducimus?</CardContent
-    >
-    <CardFooter> footer da carta </CardFooter>
+      repudiandae ducimus?
+    </CardContent>
+
+    <CardFooter class="flex-col gap-2">
+      Grau de Dificuldade na Matéria
+      <Slider class="max-w-[50%]" :default-value="[25]" :max="100" :step="25" />
+    </CardFooter>
   </Card>
 </template>
